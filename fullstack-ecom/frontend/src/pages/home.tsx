3e1ca1786b393c6d47951711c40db20e96cb29a9
Advaita <148793147+01adv@users.noreply.github.com>
2024-02-12 20:20:53 +0000

    home section remaining

--- fullstack-ecom/frontend/src/pages/home.tsx
+++ fullstack-ecom/frontend/src/pages/home.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <section></section>
+
+      <h1>Latest Products
+        <Link to='/' className='findmore'>More</Link>
+      </h1>
+
+    </div>
   )
 }
 
