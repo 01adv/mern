8a692d0961bd3891f82dcfba4015198ee6effece
Advaita <148793147+01adv@users.noreply.github.com>
2024-02-12 11:53:47 +0000

    done upto lazy loading

---
+++ fullstack-ecom/frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import {lazy} from "react";
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+const Home = lazy(() => import("./pages/home"))
+const Search = lazy(() => import('./pages/search'))
+const Cart = lazy(() => import('./pages/search'))
+
+const App = () => {
+  return (
+    <Router>
+      {/* header */}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/search" element={<Search/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
