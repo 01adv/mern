b69e7d57b51c2876e72be52c6552bdf37d46d32e
Advaita <148793147+01adv@users.noreply.github.com>
2024-03-01 21:03:41 +0530

    backend cors policy default activated

--- fullstack-ecom/backend/dist/app.js
+++ fullstack-ecom/backend/dist/app.js
@@ -1,17 +1,18 @@
 import express from "express";
 import { connectDB } from "./utils/features.js";
 import { errorMiddleware } from "./middlewares/error.js";
-import NodeCache from 'node-cache';
-import { config } from 'dotenv';
+import NodeCache from "node-cache";
+import { config } from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
 // importing routes
 import userRoutes from "./routes/user.js";
 import productRoute from "./routes/product.js";
-import orderRoute from './routes/order.js';
-import paymentRoute from './routes/payment.js';
-import dashboardRoute from './routes/stats.js';
-import morgan from "morgan";
+import orderRoute from "./routes/order.js";
+import paymentRoute from "./routes/payment.js";
+import dashboardRoute from "./routes/stats.js";
 config({
-    path: "../.env"
+    path: "../.env",
 });
 const port = process.env.PORT || 4000;
 const mongoURI = process.env.MONGO_URI || "";
@@ -20,6 +21,7 @@ export const myCache = new NodeCache();
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 app.get("/", (req, res) => {
     res.send("API Working with /api/v1");
 });
@@ -29,7 +31,7 @@ app.use("/api/v1/product", productRoute);
 app.use("/api/v1/order", orderRoute);
 app.use("/api/v1/payment", paymentRoute);
 app.use("/api/v1/dashboard", dashboardRoute);
-app.use('/uploads', express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 app.use(errorMiddleware);
 app.listen(port, () => {
     console.log(`Express is working on http://localhost:${port}`);
